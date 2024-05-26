fa137b3365ddd66ef3851015022a51d159268b45
Sahith-03 <cs21b1048@iiitdm.ac.in>
2024-05-26 17:18:55 +0530

    Token Issue

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    githubToken: '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'
-};
+  production: false,
+  githubToken: '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'
+}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { forkJoin, of } from 'rxjs';
 import { map, mergeMap,catchError } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
